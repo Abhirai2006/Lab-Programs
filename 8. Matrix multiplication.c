49f9f7504557b513928df4a0e2478ceddbcf3f94
Abhirai2006 <abhirai2006@gmail.com>
2025-07-16 00:44:39 +0530

    Update 8. Matrix multiplication.c

--- 8. Matrix multiplication.c
+++ 8. Matrix multiplication.c
@@ -34,7 +34,7 @@ int main(){
     for(i=0; i<r1; i++){
         for(j=0; j<c2; j++){
             C[i][j]=0;
-            for(k=0; k<r1; k++){
+            for(k=0; k<r2; k++){
                 C[i][j] =C[i][j] + A[i][k] * B[k][j];
             }
         }
